77bbb468e47190bdc419c4689c6fe1f5dc053fc5
Ross Jones <ross@servercode.co.uk>
2013-07-22 11:02:30 +0100

    Allow previewing of ODS files

--- ckanext/datapreview/transform/__init__.py
+++ ckanext/datapreview/transform/__init__.py
@@ -5,6 +5,6 @@ from ckanext.datapreview.transform.tabular_transform import TabularTransformer
 register_transformer({
         "name": "csv",
         "class": TabularTransformer,
-        "extensions": ["csv", "tsv", "xls"],
+        "extensions": ["csv", "tsv", "xls", "ods"],
         "mime_types": ["text/csv", "text/comma-separated-values", "application/excel", "application/vnd.ms-excel"]
     })
