18c3df9b24f311ca05f8d225373e74272d74d42e
David Read <david.read@hackneyworkshop.com>
2015-08-03 17:37:07 +0100

    Add details of truncation below the preview box (extra_text). However it is not yet working.

--- ckanext/datapreview/transform/plain_transform.py
+++ ckanext/datapreview/transform/plain_transform.py
@@ -1,13 +1,9 @@
 """Data Proxy - Plain transformation adapter"""
-import urllib2
 from ckanext.datapreview.transform.base import Transformer
 from ckanext.datapreview.lib.errors import ResourceError
-try:
-    import json
-except ImportError:
-    import simplejson as json
 
 MAX_TEXT_SIZE = 8192
+
 
 class PlainTransformer(Transformer):
     """
@@ -18,25 +14,30 @@ class PlainTransformer(Transformer):
 
     def __init__(self, resource, url, query):
         self.size = int(query.get('length', 0))
+        self.from_archive = query.get('archived', False)
         super(PlainTransformer, self).__init__(resource, url, query)
 
     def transform(self):
         handle = self.open_data(self.url)
         if not handle:
             raise ResourceError("Remote resource missing",
-                "Unable to load the remote resource")
+                                "Unable to load the remote resource")
 
         data = handle.read(MAX_TEXT_SIZE)
+        extra_text = ''
         if self.size and (self.size > MAX_TEXT_SIZE):
-            data += "... [output truncated]"
+            data += "\n... [output truncated]"
+            extra_text = 'Only the first %sk shown - download it for the full'\
+                         ' file.' % int(MAX_TEXT_SIZE/1024.0)
 
         data = data.decode('utf-8', 'ignore')
         result = {
                     "fields": ["data"],
-                    "data": [["%s" % (data)]]
+                    "data": [["%s" % (data)]],
+                    "extra_text": extra_text,
+                    "archived": "This file is previewed from the data.gov.uk archive." if self.from_archive else ""
                   }
 
         self.close_stream(handle)
 
         return result
-
